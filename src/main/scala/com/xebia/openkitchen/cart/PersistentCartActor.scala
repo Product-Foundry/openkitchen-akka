f953eaf9acee1bc30208608229eab01999a3bd8c
upeter <peter.urs@gmail.com>
2014-11-24 14:53:21 +0100

    Starting point: Implementation of an in-memory Cart Actor (SimpleCartActor)

--- src/main/scala/com/xebia/openkitchen/cart/PersistentCartActor.scala
+++ src/main/scala/com/xebia/openkitchen/cart/PersistentCartActor.scala
@@ -25,62 +25,28 @@ object PersistentCartActor {
 class PersistentCartActor extends PersistentActor with ActorLogging with ActorContextProductRepoSupport {
   import PersistentCartActor._
   import productRepo._
-  override def persistenceId = context.self.path.name
+  override def persistenceId = ???
   var cart = CartItems()
 
   val receiveCommand: Receive = {
-    case AddToCartRequest(itemId) => {
-      doWithItem(itemId) { item =>
-        persist(ItemAddedEvent(itemId)) { evt =>
-          updateStateAndPublish(evt)
-          sender ! cart.items
-        }
-      }
-    }
-    case RemoveFromCartRequest(itemId) => {
-      doWithItem(itemId) { item =>
-        persist(ItemRemovedEvent(itemId)) { evt =>
-          updateStateAndPublish(evt)
-          sender ! cart.items
-        }
-      }
-    }
-    case GetCartRequest => {
-      sender ! cart.items
-    }
+    case AddToCartRequest(itemId) => ???
+    case RemoveFromCartRequest(itemId) => ???
+    case GetCartRequest => ???
     case OrderRequest => {
       if (cart.isEmpty) {
         sender ! OrderProcessingFailed
       } else {
-        //real order processing is skipped
-        val orderId: UUID = UUID.randomUUID()
-        persist(CartCheckedoutEvent(orderId)) { evt =>
-          updateStateAndPublish(evt)
-          sender ! OrderProcessed(orderId.toString)
-        }
+        ???
       }
     }
   }
 
-  def updateState(event: Event): Unit = {
-    event match {
-      case ItemAddedEvent(itemId) =>
-        cart = cart.update(productMap(itemId))
-      case ItemRemovedEvent(itemId) =>
-        cart = cart.remove(productMap(itemId))
-      case CartCheckedoutEvent(_) =>
-        cart = cart.clear()
-    }
-  }
+  def updateState(event: Event): Unit = ???
 
-  val receiveRecover: Receive = {
-    case e: Event =>
-      log.info(s"recovery: got $e")
-      updateState(e)
-    case t: RecoveryCompleted =>
-      log.info(s"recovery completed; setting receive timeout")
-  }
+  def receiveRecover: Receive = ???
 
+  
+  
   private def doWithItem(itemId: String)(item: Device => Unit) = {
     val device = productRepo.productMap.get(itemId) match {
       case Some(device) => item(device)
